f0f0e176ff946b585ec184803e3c8f9b028fe20b
Ravi Sharma <sraa7877@gmail.com>
2023-11-18 15:36:47 +0530

    done

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, jsonify, g, request, session, redirect, url_for
 from bson import ObjectId, json_util
+from flask_pymongo import PyMongo
 from pymongo import MongoClient 
 from datetime import datetime
 from werkzeug.security import check_password_hash,generate_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import bcrypt
+from job_posting.job_posting import JobPosting
+
+from user.user import User
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -22,168 +25,6 @@ def get_db():
     if 'db' not in g:
         g.db = MongoClient(app.config['MONGO_URI'])
     return g.db
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class User:
-    def __init__(self, _id, username, password, img=None, name=None, desc=None, status=None, is_active=True):
-        self._id = _id
-        self.username = username
-        self.password = password
-        self.img = img
-        self.name = name
-        self.desc = desc
-        self.status = status
-        self.is_active = is_active
-
-    def to_dict(self):
-        return {
-            'user_id': str(self._id) if self.user_id is not None else None,
-            'username': self.username,
-            'password': self.password,  # Note: Storing passwords in plaintext is not recommended in production.
-            'img': self.img,
-            'name': self.name,
-            'desc': self.desc,
-            'status': self.status,
-            'is_active': self.is_active,
-        }
-        
-        
-    
-    def _hash_password(self, password):
-        if password is not None:
-            if isinstance(password, str):  # Check if password is a string
-                password_bytes = password.encode('utf-8')
-                hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-                return hashed_password.decode('utf-8')  # Decode bytes to string before storing
-            elif isinstance(password, bytes):  # Check if password is already in bytes
-                raise ValueError("Password must be a string")
-            else:
-                raise ValueError("Password must be a string or bytes")
-        else:
-            return None
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password) if self.password is not None else False
-
-    def get_id(self):
-        return str(self._id) if self._id is not None else None
-
-    @property
-    def is_authenticated(self):
-        # Add your authentication logic here, e.g., return True if the user is authenticated
-        return True
-
-    @staticmethod
-    def get_user_by_id(user_id, user_collection):
-        user_data = user_collection.find_one({'_id': ObjectId(user_id)})
-        if user_data:
-            return User(
-                _id=str(user_data['_id']),
-                username=user_data['username'],
-                password=user_data['password'],
-                # Add other user attributes as needed
-            )
-        return None
-    
-    def __repr__(self):
-        return f"User(username='{self.username}', status='{self.status}', is_active={self.is_active})"
-
-
-
-class JobSeeker:
-    def __init__(self, name, status=False, skills=None, experience=None, bio=None, availability=None):
-        self.name = name
-        self.status = status
-        self.skills = skills if skills else []
-        self.experience = experience
-        self.bio = bio
-        self.availability = availability
-        self.applications = []
-
-    def to_dict(self):
-        return {
-            'name': self.name,
-            'status': self.status,
-            'skills': self.skills,
-            'experience': self.experience,
-            'bio': self.bio,
-            'availability': self.availability.isoformat() if self.availability else None,
-        }
-
-
-
-
-class JobPosting:
-    def __init__(self, job_title, status='Open', start_date=None, end_date=None, hiring_manager=None, skill_sets=None, job_description=None):
-        self.job_title = job_title
-        self.status = status
-        self.start_date = start_date
-        self.end_date = end_date
-        self.hiring_manager = hiring_manager
-        self.skill_sets = skill_sets if skill_sets else []  # Assume skill_sets is a list of strings
-        self.job_description = job_description
-
-    def to_dict(self):
-        return {
-            'job_title': self.job_title,
-            'status': self.status,
-            'start_date': str(self.start_date) if self.start_date else None,
-            'end_date': str(self.end_date) if self.end_date else None,
-            'hiring_manager': self.hiring_manager,
-            'skill_sets': self.skill_sets,
-            'job_description': self.job_description,
-        }
-
-
-class Application:
-    def __init__(self, job_seeker_id, job_posting_id, status='Pending', details=None):
-        self.job_seeker_id = job_seeker_id
-        self.job_posting_id = job_posting_id
-        self.status = status
-        self.details = details or {}
-
-    def to_dict(self):
-        return {
-            'job_seeker_id': self.job_seeker_id,
-            'job_posting_id': self.job_posting_id,
-            'status': self.status,
-            'details': self.details,
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -291,35 +132,35 @@ def create_job_posting():
 
     return jsonify(message="Job posting created successfully.")
 
-# @app.route('/update-job-posting/<string:job_posting_id>', methods=['PUT'])
-# def update_job_posting(job_posting_id):
-#     data = request.get_json()
+@app.route('/update-job-posting/<string:job_posting_id>', methods=['PUT'])
+def update_job_posting(job_posting_id):
+    data = request.get_json()
 
-#     if data:
-#         db = get_db()
+    if data:
+        db = get_db()
         
-#         skill_sets_data = data.get('skill_sets', [])
-#         skill_sets = [SkillSet(name=skill['name'], description=skill.get('description')) for skill in skill_sets_data]
+        skill_sets_data = data.get('skill_sets', [])
+        skill_sets = [SkillSet(name=skill['name'], description=skill.get('description')) for skill in skill_sets_data]
 
-#         result = db.linkme.job_postings.update_one(
-#             {'_id': ObjectId(job_posting_id)},
-#             {'$set': {
-#                 'job_title': data.get('job_title'),
-#                 'status': data.get('status', 'Open'),
-#                 'start_date': data.get('start_date'),
-#                 'end_date': data.get('end_date'),
-#                 'hiring_manager': data.get('hiring_manager'),
-#                 'skill_sets': [skill.to_dict() for skill in skill_sets],
-#                 'job_description': data.get('job_description'),
-#             }}
-#         )
+        result = db.linkme.job_postings.update_one(
+            {'_id': ObjectId(job_posting_id)},
+            {'$set': {
+                'job_title': data.get('job_title'),
+                'status': data.get('status', 'Open'),
+                'start_date': data.get('start_date'),
+                'end_date': data.get('end_date'),
+                'hiring_manager': data.get('hiring_manager'),
+                'skill_sets': [skill.to_dict() for skill in skill_sets],
+                'job_description': data.get('job_description'),
+            }}
+        )
 
-#         if result.modified_count > 0:
-#             return jsonify({'message': 'Job Posting updated successfully'}), 200
-#         else:
-#             return jsonify({'error': 'Job Posting not found or not modified'}), 404
+        if result.modified_count > 0:
+            return jsonify({'message': 'Job Posting updated successfully'}), 200
+        else:
+            return jsonify({'error': 'Job Posting not found or not modified'}), 404
 
-#     return jsonify({'error': 'Invalid data provided'}), 400
+    return jsonify({'error': 'Invalid data provided'}), 400
 
 @app.route('/delete-job-posting/<string:job_posting_id>', methods=['DELETE'])
 def delete_job_posting(job_posting_id):
@@ -608,5 +449,5 @@ def teardown_db(exception=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
---
+++ job_posting/job_posting.py
@@ -0,0 +1,20 @@
+class JobPosting:
+    def __init__(self, job_title, status='Open', start_date=None, end_date=None, hiring_manager=None, skill_sets=None, job_description=None):
+        self.job_title = job_title
+        self.status = status
+        self.start_date = start_date
+        self.end_date = end_date
+        self.hiring_manager = hiring_manager
+        self.skill_sets = skill_sets if skill_sets else []  # Assume skill_sets is a list of strings
+        self.job_description = job_description
+
+    def to_dict(self):
+        return {
+            'job_title': self.job_title,
+            'status': self.status,
+            'start_date': str(self.start_date) if self.start_date else None,
+            'end_date': str(self.end_date) if self.end_date else None,
+            'hiring_manager': self.hiring_manager,
+            'skill_sets': self.skill_sets,
+            'job_description': self.job_description,
+        }
